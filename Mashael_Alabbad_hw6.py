f958c87d59cb1cc1b9f175f606a8a60ae8d3f0a2
Mashael2 <malabbad1@mail.weber.edu>
2017-03-26 16:59:05 -0600

    First commit

---
+++ Mashael_Alabbad_hw6.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+
+
+# Main Function
+def main():
+    """
+    Test Function.
+    """
+
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+exit (0)
