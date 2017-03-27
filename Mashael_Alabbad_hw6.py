1fb4d2dcd866c6224aa0d5a3271e0f3df5d8a6ee
Mashael2 <malabbad1@mail.weber.edu>
2017-03-26 23:02:35 -0600

    Updated

--- Mashael_Alabbad_hw6.py
+++ Mashael_Alabbad_hw6.py
@@ -1,5 +1,50 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+import urllib
+import urllib.request
+import re
+from collections import Counter,OrderedDict
+
+
+
+def help():
+    """
+    Display usage
+    """
+    print("Usage is: ./Mashael_Alabbad_hw6.py <file Input>")
+
+
+
+def check():
+    """
+    Check for the file
+    if there is no file call help function
+    """
+    if len(sys.argv) == 1:
+        help()
+        exit(1)
+    else:
+        url = sys.argv[1]
+        return url
+
+
+
+def getError(url):
+    """
+    Get all errors from URL
+    """
+    sourceFile = urllib.request.urlopen(url)
+    content = sourceFile.read().decode()
+
+    theList = re.findall(r'(?:\[.*?\]) (?:\[error\]) (?:\[.*?\]) (.*)', content)
+    #Find the top 25 errors
+    errors =OrderedDict(Counter(theList).most_common(25))
+
+    print("*** Top 25 page errors ***")
+    for page, value in errors.items():
+        print('Count: {} Page: {}'.format(value, page))
+
+    return 
 
 
 
@@ -8,11 +53,14 @@ def main():
     """
     Test Function.
     """
+    url = check()
+    getError(url)
 
+# "http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test"
 
 
 if __name__ == "__main__":
     # Call Main
     main()
 
-exit (0)
+    exit(0)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Assignment6
+Python script to find the top 25 errors from:
+"http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test"
